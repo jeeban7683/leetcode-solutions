a74644429206ca6a138dcf10ae50c4e786ff52b4
jeeban7683 <jeevanjyotigochhayatjitu@gmail.com>
2026-07-25 20:04:00 +0530

    Time: 25 ms (46.05%), Space: 50.6 MB (10.95%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+    
+    ListNode current = head;
+    ArrayList<Integer> list = new ArrayList<>();
+
+    while(current != null ){
+
+       list.add(current.val);
+       current = current.next;
+
+    }
+    int[] ans = new int[list.size()];
+    Stack<Integer> st = new Stack<>();
+
+    for (int i = list.size() - 1; i >= 0; i--){   
+        while(!st.isEmpty() && st.peek() <= list.get(i)){
+            st.pop();
+        }
+        if(st.isEmpty()){
+            ans[i] = 0;
+        }else{
+            ans[i] = st.peek();
+        }
+        st.push(list.get(i));
+    }
+    return ans ;
+    }
+}
