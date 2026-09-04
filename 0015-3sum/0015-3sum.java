5f80a5d545d1b482bb80b065b4875a65c45a1e15
jeeban7683 <jeevanjyotigochhayatjitu@gmail.com>
2026-09-04 20:51:31 +0530

    Time: 794 ms (10.15%), Space: 59.9 MB (16.28%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,56 +1,37 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        // Create answer list
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // If size less than 3
-        if (nums.length < 3) {
-            return ans;
+        List<List<Integer>> list = new ArrayList<>();
+        if(nums.length < 3){
+            return list;
         }
-
-        // Sort array
         Arrays.sort(nums);
 
-        // Use set to avoid duplicate triplets
         Set<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+          int left = i+1;
+          int right = nums.length -1;
+        
+        while(left < right){
 
-        // Fix one element
-        for (int i = 0; i < nums.length - 2; i++) {
+           int sum = nums[i] + nums[left] + nums[right];
 
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            // Two pointer
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                // Triplet found
-                if (sum == 0) {
-
-                    set.add(Arrays.asList(
-                            nums[i],
-                            nums[left],
-                            nums[right]
-                    ));
-
-                    left++;
-                    right--;
-                }
-
-                // Need bigger sum
-                else if (sum < 0) {
-                    left++;
-                }
-
-                // Need smaller sum
-                else {
-                    right--;
-                }
-            }
+           if(sum == 0){
+            set.add(Arrays.asList(
+                nums[i],
+                nums[left],
+                nums[right]
+            ));
+            left++;
+            right--;
+           }else if (sum < 0){
+            left ++;
+           }else{
+            right--;
+           }
         }
 
+         
+        }
         return new ArrayList<>(set);
     }
 }
